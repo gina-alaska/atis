ab1da3eab7abe118f58f53098876ca91cf0a7c47
Will Fisher <will@teknofire.net>
2012-10-23 11:52:48 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Atis::Application.config.secret_token = '1daf0fee21999500e1baa0f39a3fd1bac648b6ba5982eda932cbc2b168d7f2b57c3c8e1264a188cbe44c2c83a60f808a881647210ce113388b9367ce8d585ce0'
