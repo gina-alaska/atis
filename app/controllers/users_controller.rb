c8bf9a0df2d515b060dd3360c33c899fe465a9d2
Will Fisher <will@teknofire.net>
2012-10-28 14:16:07 -0800

    edit forms for user permissions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,25 @@ class UsersController < ApplicationController
   def edit
   end
   
+  def update
+    @user.roles = user_params[:roles]
+    respond_to do |format|
+      if @user.save
+        format.html do
+          flash[:success] = "Updated permissions for #{@user.name}"
+          redirect_to users_path
+        end
+      end
+    end
+  end
+
   protected
+  
+  def user_params
+    p = params[:user].slice(:roles, :agency_id)
+    p['roles'] = p['roles'].collect { |k,v| Role.where(name: k).first if v.to_i == 1 }.compact
+    p
+  end
   
   def fetch_user
     @user = User.find(params[:id])
