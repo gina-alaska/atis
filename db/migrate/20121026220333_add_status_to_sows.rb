ce6a2b815dc25f07c7fe3cdeded79a8859bfc1de
Will Fisher <will@teknofire.net>
2012-10-29 08:59:03 -0800

    fixing typo in migration

--- db/migrate/20121026220333_add_status_to_sows.rb
+++ db/migrate/20121026220333_add_status_to_sows.rb
@@ -2,8 +2,8 @@ class AddStatusToSows < ActiveRecord::Migration
   def change
     add_column :sows, :state, :string
     add_column :sows, :created_by, :integer
-    add_column :sows, :submited_on, :datetime
-    add_column :sows, :submited_by, :integer
+    add_column :sows, :submitted_on, :datetime
+    add_column :sows, :submitted_by, :integer
     add_column :sows, :reviewed_by, :integer
     add_column :sows, :accepted_on, :datetime
     add_column :sows, :rejected_on, :datetime
