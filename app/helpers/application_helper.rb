4af9e7f0d1b424f6c3e93373d985e943ccc8fcb6
Will Fisher <will@teknofire.net>
2012-11-20 11:56:06 -0900

    clean up the header

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,13 @@ module ApplicationHelper
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
   end
+  
+  def active_dashboard_class(dashboard)
+    'active' if current_page?(root_url) and current_user.active_dashboard.to_sym == dashboard.to_sym
+  end
+  
+  def active_admin_class
+    return 'active' if %w{ users groups disciplines }.include? params[:controller]
+    return active_dashboard_class(:admin)
+  end
 end
